bfa8c35dbec859fb611231925bb30c06365627b0
MarkChase3 <103606684+MarkChase3@users.noreply.github.com>
2022-07-03 12:16:22 +0000

    enemies follow the player

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // I don't know how this piece of code works, and I don't even want to know that, but it detects if the user is using a mobile or desktop device.
 let mobile = false;
 (function(a, b) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) mobile = b })(navigator.userAgent || navigator.vendor || window.opera, 'https://game.markchase3.repl.co');
@@ -19,7 +20,6 @@ function loadImage(path) {
   buffer.src = path;
   return buffer;
 }
-
 //input settings
 let mouse = { x: 0, y: 0, pressed: 0 };
 if (mobile) {
@@ -62,6 +62,9 @@ function openFullscreen() {
     document.documentElement.msRequestFullscreen();
   }
 }
+function aabbCollision(ax, ay, aw, ah, bx, by, bw, bh) {
+  return (ax + aw > bx && ay + ah > by && bx + bw > ax && by + bh > ay);
+}
 
 // special effects in the background
 let stars = [], lastStarSpawn = 0;
@@ -95,12 +98,65 @@ function updateStars() {
   stars.forEach(updateStar);
 }
 
+//enemies
+let enemies = [];
+function createEnemie(imgPath, y, speed) {
+  let enemie = {};
+  enemie.img = loadImage(imgPath);
+  enemie.frames = [[1, 1, 16, 16], [18, 1, 16, 16]];
+  enemie.currFrame = 0;
+  enemie.y = y;
+  enemie.x = canvas.width;
+  enemie.speed = speed;
+  enemie.gotos = [];
+  return enemie
+}
+function updateEnemie(enemie) {
+  if (enemie.x > canvas.width / 10 * 9) enemie.x--;
+  ctx.drawImage(enemie.img,
+    enemie.frames[enemie.currFrame][0],
+    enemie.frames[enemie.currFrame][1],
+    enemie.frames[enemie.currFrame][2],
+    enemie.frames[enemie.currFrame][3],
+    enemie.x, enemie.y,
+    enemie.frames[enemie.currFrame][2],
+    enemie.frames[enemie.currFrame][3],
+  );
+}
+function updateEnemies() {
+  enemies.forEach(updateEnemie);
+  enemiesFollowPlayer();
+}
+function enemieDontCollideWithOthers(enemie1, i) {
+  for (let j = 0; j < enemies.length; j++) {
+    if (i !== j) {
+      if (enemies[j].y + 16 > enemies[i].y && enemies[i].y + 16 > enemies[j].y) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+function enemiesFollowPlayer() {
+ let y = player.y;
+ for(let i=0;i<enemies.length;i++){
+  if (enemies[i].x <= canvas.width / 10 * 9 && Math.abs(player.y - enemies[i].y)>enemies[i].speed) {
+      let yBuffer = enemies[i].y;
+      enemies[i].y += ((player.y - enemies[i].y) > 0 ? enemies[i].speed : -enemies[i].speed);
+      if (!enemieDontCollideWithOthers(enemies[i], i, i)) {
+        enemies[i].y = yBuffer;
+      }
+  }
+}
+}
 //player
 let player = {};
 function createPlayer() {
   player.img = loadImage('playerShip.png');
-  player.frames = [[1, 1, 16, 16], [1, 18, 16, 16]];
+  player.frames = [[1, 1, 16, 16], [18, 1, 16, 16]];
   player.currFrame = 0;
+  player.lastFrame = 0;
+  player.frameChangeFrequency = 100;
   player.x = canvas.width / 10;
   player.y = canvas.height / 2 - player.frames[0][3] / 2;
   player.lastShoot = 0;
@@ -110,14 +166,14 @@ function createPlayer() {
   player.shoots = [];
   player.moving = false;
   player.gradientSize = 16;
-  player.gradient = ctx.createRadialGradient(player.x + player.frames[player.currFrame][2]/2, player.y + player.frames[player.currFrame][3]/2, player.gradientSize / 10, player.x + player.frames[player.currFrame][2]/2, player.y + player.frames[player.currFrame][3]/2, player.gradientSize);
+  player.gradient = ctx.createRadialGradient(player.x + player.frames[player.currFrame][2] / 2, player.y + player.frames[player.currFrame][3] / 2, player.gradientSize / 10, player.x + player.frames[player.currFrame][2] / 2, player.y + player.frames[player.currFrame][3] / 2, player.gradientSize);
   player.gradient.addColorStop(0, 'rgb(0,162,232)')
   player.gradient.addColorStop(1, 'rgba(0,162,232,0)');
   console.log(player.y);
 }
 function drawPlayer() {
   ctx.beginPath();
-  ctx.arc(player.x + player.frames[player.currFrame][2]/2, player.y + player.frames[player.currFrame][3]/2, player.gradientSize, 0, 2 * Math.PI);
+  ctx.arc(player.x + player.frames[player.currFrame][2] / 2, player.y + player.frames[player.currFrame][3] / 2, player.gradientSize, 0, 2 * Math.PI);
   ctx.fillStyle = player.gradient;
   ctx.fill();
   ctx.drawImage(player.img, player.frames[player.currFrame][0],
@@ -128,6 +184,10 @@ function drawPlayer() {
     player.frames[player.currFrame][2],
     player.frames[player.currFrame][3],
   );
+  if (Date.now() - player.lastFrame > player.frameChangeFrequency) {
+    player.lastFrame = Date.now();
+    player.currFrame = (player.currFrame == 0 ? 1 : 0);
+  }
 }
 function movePlayer() {
   if (Math.abs(mouse.y - (player.y + player.frames[player.currFrame][3] / 2)) < 10) {
@@ -138,7 +198,7 @@ function movePlayer() {
   }
   if (player.moving) {
     player.y = mouse.y - player.frames[player.currFrame][3] / 2;
-  player.gradient = ctx.createRadialGradient(player.x + player.frames[player.currFrame][2]/2, player.y + player.frames[player.currFrame][3]/2, player.gradientSize / 10, player.x + player.frames[player.currFrame][2]/2, player.y + player.frames[player.currFrame][3]/2, player.gradientSize);
+    player.gradient = ctx.createRadialGradient(player.x + player.frames[player.currFrame][2] / 2, player.y + player.frames[player.currFrame][3] / 2, player.gradientSize / 10, player.x + player.frames[player.currFrame][2] / 2, player.y + player.frames[player.currFrame][3] / 2, player.gradientSize);
     player.gradient.addColorStop(0, 'rgb(0,162,232)')
     player.gradient.addColorStop(1, 'rgba(0,162,232,0)');
   }
@@ -192,7 +252,10 @@ function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     updateStars();
     updatePlayer();
+    updateEnemies();
   }
   window.requestAnimationFrame(update);
 }
 update();
+enemies.push(createEnemie('enemie1Ship.png', 80, 2))
+enemies.push(createEnemie('playerShip.png', 50, 4))
